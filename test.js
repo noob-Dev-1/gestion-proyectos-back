72cf930ceee39e8b17bf10a26f62dbcb951dfe7b
Yuliannn94 <yuliannn94@gmail.com>
2021-12-19 20:43:38 -0500

    correccion prueba unitaria 2

--- test.js
+++ test.js
@@ -9,14 +9,14 @@ dotenv.config();
 await conectarBD();
 
 const server = new ApolloServer({
-    typeDefs: tipos,
-    resolvers: resolvers,
+  typeDefs: tipos,
+  resolvers: resolvers,
 });
 
 //Prueba de creación de un usuario
 it('creates user', async () => {
-    const result = await server.executeOperation({
-        query: gql`
+  const result = await server.executeOperation({
+    query: gql`
         mutation Mutation(
           $nombre: String!
           $apellido: String!
@@ -37,78 +37,77 @@ it('creates user', async () => {
           }
         }
       `,
-        variables: {
-            nombre: 'test',
-            apellido: 'test',
-            identificacion: 'test',
-            correo: 'testing@testing.com',
-            rol: 'ADMINISTRADOR',
-            password: 'test',
-        },
-    });
+    variables: {
+      nombre: 'test',
+      apellido: 'test',
+      identificacion: 'test',
+      correo: 'testing@testing.com',
+      rol: 'ADMINISTRADOR',
+      password: 'test',
+    },
+  });
 
-    assert.equal(result.data.crearUsuario.correo, 'testing@testing.com');
+  assert.equal(result.data.crearUsuario.correo, 'testing@testing.com');
 });
 
 //Prueba de query de ese usuario creado
 it('fetches user', async () => {
-    const result = await server.executeOperation({
-        query: gql`
-        query Usuarios($filtro: FiltroUsuarios) {
+  const result = await server.executeOperation({
+    query: gql`
+        query Usuarios($filtro: filtrosUsuarios) {
           Usuarios(filtro: $filtro) {
             correo
+            }
           }
-        }
       `,
-        variables: {
-            filtro: {
-                correo: 'testing@testing.com',
-            },
-        },
-    });
+      variables: {
+        filtro: {
+          "correo": 'testing@testing.com',},
+    },
+  });
 
-    assert.equal(result.data.Usuarios.length, 1);
-    assert.equal(result.data.Usuarios[0].correo, 'testing@testing.com');
-    //Si el resultado (izquierda) es igual al correo esperado(derecha), la prueba pasa
+  assert.equal(result.data.Usuarios.length, 1);
+  assert.equal(result.data.Usuarios[0].correo, 'testing@testing.com');
+  //Si el resultado (izquierda) es igual al correo esperado(derecha), la prueba pasa
 
 });
 
 //Prueba de Eliminaciómn de ese usuario
 it('deletes user', async () => {
-    const result = await server.executeOperation({
-        query: gql`
+  const result = await server.executeOperation({
+    query: gql`
         mutation EliminarUsuario($correo: String) {
           eliminarUsuario(correo: $correo) {
             correo
           }
         }
       `,
-        variables: {
-            correo: 'testing@testing.com',
-        },
-    });
-    assert.equal(result.data.eliminarUsuario.correo, 'testing@testing.com');
-    assert.equal()
-    //Si el resultado (izquierda) es igual al correo esperado(derecha), la prueba pasa
+    variables: {
+      "correo": 'testing@testing.com',
+    },
+  });
+  assert.equal(result.data.eliminarUsuario.correo, 'testing@testing.com');
+  assert.equal()
+  //Si el resultado (izquierda) es igual al correo esperado(derecha), la prueba pasa
 });
 
 //Prueba de query de ese usuario eliminado 
 it('fetches user after deletion', async () => {
-    const result = await server.executeOperation({
-        query: gql`
+  const result = await server.executeOperation({
+    query: gql`
         query Usuarios($filtro: FiltroUsuarios) {
           Usuarios(filtro: $filtro) {
             correo
           }
         }
       `,
-        variables: {
-            filtro: {
-                correo: 'testing@testing.com',
-            },
-        },
-    });
+    variables: {
+      filtro: {
+        "correo": 'testing@testing.com',
+      },
+    },
+  });
 
-    assert.equal(result.data.Usuarios.length, 0);
-    //Si el resultado es un objeto o arreglo vacío, la prueba pasa
+  assert.equal(result.data.Usuarios.length, 0);
+  //Si el resultado es un objeto o arreglo vacío, la prueba pasa
 });
